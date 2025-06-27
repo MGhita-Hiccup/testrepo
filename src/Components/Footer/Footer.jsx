01df7691e4dc6152e50d5963c53dd148687762c9
mahmoud-ghita <ghita101.mahmoud@gmail.com>
2025-06-27 04:07:53 -0700

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,11 +3,10 @@ import React from 'react'
 export default function Footer() {
   return (
     <>
-   <footer className="footer py-3 mt-3 text-center">
+   <footer className="footer py-1 mt-1 text-center">
     <div className='one'>
       <p>&copy; 2025 OSN+ Movies. All rights reserved.</p>
     </div>
-    <br/>
     <div className='item'>
       <a className="nav-link text-white" href="#"><i className='fab fa-facebook'></i></a>
       <a className="nav-link text-white" href="#"><i className='fab fa-instagram'></i></a>
